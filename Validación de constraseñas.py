fe12b380a0751ff102c14dee1dd7f7bf5a2ce066
Neobraingit <Neobraingit@github.com>
2022-11-07 20:23:02 +0100

    Validación de contraseñas

--- Validación de constraseñas.py
+++ Validación de constraseñas.py
@@ -1,12 +1,19 @@
-contraseña = 'Motherangel74'
+contraseña = 'Pepitogrillo'
+usuario = 'Admin'
+user = input('Introduce tu ususario: ')
 passwd = input('Introduce tu contraseña: ')
 
+
 contador = 0
-while  passwd != contraseña:
+while  passwd != contraseña and user != usuario :
+    print ('Contraseña o usuario incorrecto¡¡')
+    user = input('Introduce tu ususario: ')
     passwd = input('Introduce tu contraseña: ')
     contador += 1
+print ('Acceso conseguido¡¡¡¡')
 
-print ('Acceso conseguido¡¡')
+
+
 
 
 
