849fdc08062a029758e80b8b11105ea08a7655a9
Neobraingit <Neobraingit@github.com>
2022-12-11 17:09:42 +0100

    Cambios

--- Contador de letras.py
+++ Contador de letras.py
@@ -1,6 +1,7 @@
 frase = input('Introduce una texto: ')
 frase = frase.lower()
 letra = input('Introduce la letra a contar: ')
+letra = letra.lower()
 contador = 0
 for i in frase:
     if i in letra:
